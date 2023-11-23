3cdaf00182e255f2ea8af6ca4d56f41ed7f58e23
nicoleneza <rwegoneza12@gmail.com>
2023-11-23 08:20:02 +0200

    composition

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,23 @@
+import { useVideoConfig, useCurrentFrame } from "remotion/";
 export const MyComposition = () => {
-	return null;
+	const { durationInFrames ,fps ,width, height } = useVideoConfig();
+	const frame = useCurrentFrame()
+
+	const opacity = frame/durationInFrames;
+
+	return (
+		<div
+		style={
+			{
+				flex: 1,
+				textAlign:'center',
+				color:'white',
+				fontSize:'80px',
+				background: "black",
+				opacity,
+			}
+		}>
+			Good morning, I used a video of {width}*{height} px frame
+		</div>
+	)
 };
